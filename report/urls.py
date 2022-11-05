e93b712a7befb1eec348627b81ae158fef6ac6f7
Agung Wiyono <wiyonoagung1@gmail.com>
2022-11-05 12:47:30 +0700

    Add last month report

--- report/urls.py
+++ report/urls.py
@@ -3,5 +3,8 @@ from telegram.ext import CommandHandler
 from report import bot_func
 
 month_report_handler = CommandHandler("report", bot_func.this_month_report)
+last_month_report_handle = CommandHandler(
+    "last-report", bot_func.last_month_report
+)
 
-bot_handlers = [month_report_handler]
+bot_handlers = [month_report_handler, last_month_report_handle]
